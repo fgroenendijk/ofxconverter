26300cb29c620362e90e4827e384dc248a36cea2
Floris Groenendijk <weirdall@gmail.com>
2010-06-11 18:09:38 +0000

    Code cleanup/consistency.

--- src/ofxconverter/gui/renderer/ComboBoxCellRenderer.java
+++ src/ofxconverter/gui/renderer/ComboBoxCellRenderer.java
@@ -19,7 +19,8 @@ public class ComboBoxCellRenderer extends JComboBox implements TableCellRenderer
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
         if (isSelected) {
-            setForeground(table.getSelectionForeground()); super.setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+            super.setBackground(table.getSelectionBackground());
         } else {
             setForeground(table.getForeground()); setBackground(table.getBackground());
         }
