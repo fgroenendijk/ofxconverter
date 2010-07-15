891a221dd17ea9abb1873989fa871537d5b4ec99
Floris Groenendijk <weirdall@gmail.com>
2010-07-15 18:07:05 +0000

    Sometimes the Rabobank csv file contains 15 fields. Will now process lines made up of 15 fields. Also would skip lines that had amounts like .30. Also Fixed.

--- src/ofxconverter/module/input/Rabobank.java
+++ src/ofxconverter/module/input/Rabobank.java
@@ -17,7 +17,7 @@ import ofxconverter.structure.Transaction;
  */
 public class Rabobank extends Bank {
 
-    private static final String match = "^\"\\d*\",\".*\",\\d+,\".*\",\\d+\\.\\d+(,\".*\"){2},\\d+(,\".*\"){6}$";
+    private static final String match = "^\"\\d*\",\".*\",\\d+,\".*\",\\d*\\.\\d+(,\".*\"){2},\\d+(,\".*\"){5}.*$";
 
     public static String getMatch(){
         return match;
@@ -29,16 +29,12 @@ public class Rabobank extends Bank {
 
     protected void parseLine( String line, BankStatement bankStatement ){
 
-        Transaction transaction = new Transaction();
-
-        boolean isDebet = false;
-
         String strDigits = "\"(\\d+)\"";
         String strChars = "\"(\\w+)\"";
         String strText = "\"(.*)\"";
         String character = "\"(\\w)\"";
         String digits = "(\\d+)";
-        String amount = "(\\d*[,\\.]?\\d+[,\\.]\\d+)";
+        String amount = "(\\d*[,\\.]?\\d*[,\\.]\\d+)";
         String strChar = "\"(\\w*)\"";
         String comma = ",";
 
@@ -63,80 +59,123 @@ public class Rabobank extends Bank {
 
         if( m.find() ){
 
-            for( int i = 1; i < m.groupCount(); i++ ){
-                switch( i ){
-                    case 1:
-                        if(  isEmptyString( bankStatement.getAccount() ) ){
-                            bankStatement.setAccount( m.group(i) );
-                        }
-                        break;
-
-                    case 2:
-                        if(  isEmptyString( bankStatement.getCurrency() ) ){
-                            bankStatement.setCurrency( m.group(i) );
-                        }
-                        break;
-
-                    case 3:
-                        transaction.setInterestDate( m.group(i) );
-                        if(  isBigger( bankStatement.getDateStart(), transaction.getInterestDate() )  ){
-                            bankStatement.setDateStart( m.group(i) );
-                        }
-                        if(  isSmaller( bankStatement.getDateEnd(), transaction.getInterestDate() )  ){
-                            bankStatement.setDateEnd( m.group(i) );
-                        }
-                        break;
-                    case 4:
-                        isDebet = m.group(i).equalsIgnoreCase( "d" );
-                        break;
-                    case 5:
-                        String amountTemp = m.group(i);
-                        if( isDebet ){
-                            amountTemp = "-" + amountTemp;
-                        }
-                        transaction.setAmount(amountTemp);
-                        break;
-                    case 6:
-                        transaction.setAccount(m.group(i));
-                        break;
-                    case 7:
-                        transaction.setName(m.group(i));
-                        break;
-                    case 8:
-                        transaction.setDate( m.group(i) );
-                        break;
-                    case 9:
-                        transaction.setType( transactionType( m.group(i), isDebet ) );
-                        break;
-                    case 10:
-                        transaction.setMemo( m.group(i) + " " );
-                        break;
-                    case 11:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
-                        break;
-                    case 12:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
-                        break;
-                    case 13:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
-                        break;
-                    case 14:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
-                        break;
-                    case 15:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
-                        break;
-                    case 16:
-                        transaction.setMemo( transaction.getMemo() + m.group(i) );
-                        break;
-                }
-            }
+            Transaction transaction = processLine( m, bankStatement );
 
             bankStatement.addTransaction(transaction);
 
         }
         else{
-            bankStatement.addFailedString(line);
+          
+            // This is in the case where the line has 15 instead of 16 fields
+            p = Pattern.compile( strChars + comma +
+                                     strChars + comma +
+                                     digits + comma +
+                                     character + comma +
+                                     amount + comma +
+                                     strChar + comma +
+                                     strText + comma +
+                                     digits + comma +
+                                     strChars + comma +
+                                     strChar + comma +
+                                     strText + comma +
+                                     strText + comma +
+                                     strText + comma +
+                                     strText + comma +
+                                     strText );
+
+            m = p.matcher( line );
+
+            if( m.find() ){
+
+                Transaction transaction = processLine( m, bankStatement );
+
+                bankStatement.addTransaction(transaction);
+
+            }
+            else{
+
+                bankStatement.addFailedString(line);
+
+            }
+
         }
     }
+
+    private Transaction processLine( Matcher m, BankStatement bankStatement ){
+
+        Transaction transaction = new Transaction();
+        boolean isDebet = false;
+
+        for( int i = 1; i < m.groupCount(); i++ ){
+            switch( i ){
+                case 1:
+                    if(  isEmptyString( bankStatement.getAccount() ) ){
+                        bankStatement.setAccount( m.group(i) );
+                    }
+                    break;
+
+                case 2:
+                    if(  isEmptyString( bankStatement.getCurrency() ) ){
+                        bankStatement.setCurrency( m.group(i) );
+                    }
+                    break;
+
+                case 3:
+                    transaction.setInterestDate( m.group(i) );
+                    if(  isBigger( bankStatement.getDateStart(), transaction.getInterestDate() )  ){
+                        bankStatement.setDateStart( m.group(i) );
+                    }
+                    if(  isSmaller( bankStatement.getDateEnd(), transaction.getInterestDate() )  ){
+                        bankStatement.setDateEnd( m.group(i) );
+                    }
+                    break;
+                case 4:
+                    isDebet = m.group(i).equalsIgnoreCase( "d" );
+                    break;
+                case 5:
+                    String amountTemp = m.group(i);
+                    if( isDebet ){
+                        amountTemp = "-" + amountTemp;
+                    }
+                    transaction.setAmount(amountTemp);
+                    break;
+                case 6:
+                    transaction.setAccount(m.group(i));
+                    break;
+                case 7:
+                    transaction.setName(m.group(i));
+                    break;
+                case 8:
+                    transaction.setDate( m.group(i) );
+                    break;
+                case 9:
+                    transaction.setType( transactionType( m.group(i), isDebet ) );
+                    break;
+                case 10:
+                    transaction.setMemo( m.group(i) + " " );
+                    break;
+                case 11:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
+                    break;
+                case 12:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
+                    break;
+                case 13:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
+                    break;
+                case 14:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
+                    break;
+                case 15:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) + " " );
+                    break;
+                case 16:
+                    transaction.setMemo( transaction.getMemo() + m.group(i) );
+                    break;
+            }
+        }
+
+        return transaction;
+    }
+
 }
