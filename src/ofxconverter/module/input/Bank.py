6e3d7088257661605ba64eaae246a029bfab11d1
Floris Groenendijk <weirdall@gmail.com>
2014-04-11 14:20:42 +0000

    Added test file, started with parsing csv in python

--- src/ofxconverter/module/input/Bank.py
+++ src/ofxconverter/module/input/Bank.py
@@ -3,7 +3,13 @@ from configobj import ConfigObj
 class Bank:
 
     def checkFile( fileName ):
-        
+        file = open( fileName )
+        for line in file:
+            if ibanSearch = re.search( r'\w{2}\d{2}\w{4}\d{7}\w{0,16}', line ):
+                for column in line:
+                    if ibanSearch:
+                        ibans = re.findall(r'\w{2}\d{2}\w{4}\d{7}\w{0,16}', column.replace(' ','') );  
+        file.close()                
 
     def parseLine(self, line):
         print( line )
@@ -21,4 +27,7 @@ class Bank:
 
 if __name__ == '__main__':
     b = Bank()
-    b.readFile( '' );
+
+    b.checkFile( 'test.csv' )
+    
+    b.readFile( 'test.csv' );
