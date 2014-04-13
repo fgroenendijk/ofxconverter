41272717ec71c52c3f1c945529a01ad702c538ea
Floris Groenendijk <weirdall@gmail.com>
2014-04-13 17:14:20 +0200

    Parsing cvs files to see which bank it belongs to

--- src/ofxconverter/module/input/Bank.py
+++ src/ofxconverter/module/input/Bank.py
@@ -1,15 +1,55 @@
 from configobj import ConfigObj
+from os.path import expanduser
+import re
+import csv
 
 class Bank:
 
-    def checkFile(self, fileName):
-        file = open( fileName )
-        for line in file:
-            if ibanSearch = re.search( r'\w{2}\d{2}\w{4}\d{7}\w{0,16}', line ):
-                for column in line:
-                    if ibanSearch:
-                        ibans = re.findall(r'\w{2}\d{2}\w{4}\d{7}\w{0,16}', column.replace(' ','') );  
-        file.close()                
+    def checkIban(self, iban, ibanSet):
+        ibanCount = 1;
+        for oldIbanTuple in ibanSet:
+            print( 'tuple:', oldIbanTuple )
+            (oldIban, count) = oldIbanTuple
+            print( 'count', count, 'oldIban', oldIban )
+            if oldIban == iban:
+                ibanSet.remove( oldIbanTuple )
+                ibanCount = count + 1
+                break;
+
+        ibanSet.add( (iban, ibanCount) )
+
+        highestCount = 0
+        ibanFound = []
+        if( len( ibanSet ) > 1 ):
+            for ibanTuple in ibanSet:
+                (iban, count) = ibanTuple
+                if count > highestCount:
+                    highestCount = count
+
+            for ibanTuple in ibanSet:
+                (iban, count) = ibanTuple
+                if count == highestCount:
+                    ibanFound.append( iban )    
+
+        return ibanFound
+
+    def searchMainIban(self, fileName):
+        ibans = set([])
+        file = csv.reader( open(fileName) )
+        for row in file:
+            line = ','.join( row )
+            match = re.search( r'\w{2}\d{2}\w{4}\d{7}\w{0,16}', line.replace( ' ', '' ) )
+            if match:
+                for column in row:
+                    search = re.search(r'(\w{2}\d{2}\w{4}\d{7}\w{0,16})', column.replace(' ','') )
+                    if search:
+                        print( 'search:', search )
+                        mainIban = self.checkIban( search.group(0), ibans )
+                if len( mainIban ) == 1:
+                    break;
+                        
+        print( 'ibans:', ibans )
+        print( 'mainIban:', mainIban )
 
     def parseLine(self, line):
         print( line )
@@ -17,7 +57,7 @@ class Bank:
     def readFile(self, fileName):
         file = open( fileName )
         for line in file:
-            parseLine( line )
+            self.parseLine( line )
         file.close()
 
     def __init__(self):
@@ -28,6 +68,13 @@ class Bank:
 if __name__ == '__main__':
     b = Bank()
 
-    b.checkFile( 'test.csv' )
+    home = expanduser("~")
+    print( 'home:', home )
+
+    mainIban = b.searchMainIban( 'test.csv' )
+
+    # If mainIban contains more than one iban,
+    # let the user select which one is the main iban
+    # Otherwise we know the bank this csv belongs to
     
-    b.readFile( 'test.csv' );
+    b.readFile( 'test.csv' )
