4f0472d5b080f4442d3f468c7218771755fa2fcf
Floris Groenendijk <f.groenendijk@bluebillywig.com>
2014-04-14 17:23:14 +0200

    Forgot the transfer types, readded

--- src/ofxconverter/structure/Transaction.py
+++ src/ofxconverter/structure/Transaction.py
@@ -44,13 +44,60 @@ class Transaction:
     
     @memo.setter
     def memo(self, memo):
-            self.__memo = re.sub("\s{2,}"," ",memo.rstrip()) 
+            self.__memo = re.sub("\s{2,}"," ",memo.rstrip())
+
+    @property
+    def rawType(self):
+        return self.__type
+
+    @rawType.setter
+    def rawType(self, rawType, isDebet):
+        returnType = "OTHER"
+
+        # Machtiging
+        if rawType.equalsIgnoreCase( "MA" ): 
+            returnType = "DIRECTDEBIT"
+
+        # Telebankieren
+        elif rawType.equalsIgnoreCase( "TB"): 
+            returnType = "PAYMENT"
+
+        # Betaalautomaat
+        elif rawType.equalsIgnoreCase( "BA"): 
+            returnType = "POS"
+
+        # Geldautomaat (pin)
+        elif rawType.equalsIgnoreCase( "GA"): 
+            returnType = "ATM"
+
+        # Overschrijving
+        elif rawType.equalsIgnoreCase( "OV"): 
+            if isDebet:
+                returnType = "DEBIT"
+            else:
+                returnType = "CREDIT"
+
+        # Bijschrijving
+        elif rawType.equalsIgnoreCase( "BY"): 
+            if isDebet:
+                returnType = "DEBIT"
+            else:
+                returnType = "CREDIT"
+
+        # Diversen
+        elif rawType.equalsIgnoreCase( "DA"):
+            if isDebet:
+                returnType = "DEBIT"
+            else:
+                returnType = "CREDIT"
+
+        self.__type = returnType 
 
     def __init__(self):
         self.__interestDate = 0
         self.__date = 0
         self.__amount = ""
         self.__memo = ""
+        self.__type = "" 
         self.name = ""
         self.account = ""
-        self.type = "OTHER" 
