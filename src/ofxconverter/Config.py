53e27802a61670018b2b3f0c647deb272c8a07cf
Floris Groenendijk <f.groenendijk@bluebillywig.com>
2014-04-15 17:27:40 +0200

    Getting there

--- src/ofxconverter/Config.py
+++ src/ofxconverter/Config.py
@@ -17,17 +17,15 @@ class Config:
     # 5 name
     # 6 account
     # 7 type
+    # 8 debet/credit
 
     def writeNewConfig(self,filename):
         config = [
-            'NL94INGB = 1,2,3,4,5,6,7',
-            'NL95INGB = 1,2,3,4,5,6,7'
+            'NL94INGB = 1,1,7,9,2,3,5,6',
+            'NL95INGB = 1,2,3,4,5,6,7,8'
             ]            
 
-        print( config )
-
         cfg = ConfigObj( config )
-        print( cfg )
         cfg.filename = filename
         cfg.write()
 
@@ -35,14 +33,40 @@ class Config:
         home = expanduser("~")
         configDir = join( home, '.ofxconverter' )
 
-        makedirs( configDir, exist_ok=True )        
-        configFile = join( configDir, 'banks.config' )
+        makedirs( configDir, exist_ok=True )       
+        self.configFile = join( configDir, 'banks.config' )
         
-        if not exists( configFile ) or isfile( configFile ):
-            print( 'Write new config file:', configFile )
-            self.writeNewConfig( configFile )
+        if not exists( self.configFile ) or isfile( self.configFile ):
+            print( 'Write new config file:', self.configFile )
+            self.writeNewConfig( self.configFile )
+
+    def getCurrentBank(self, bank):
+        cfg = ConfigObj( self.configFile )
+        for cfgBank, cfgBankValue in cfg.iteritems():
+            if cfgBank == bank:
+                for fieldNumber in cfgBankValue:
+                    if not 'interestDate' in self.fields:
+                        self.fields.append( [ 'interestDate', fieldNumber ] )
+                    elif not 'date' in self.fields:
+                        self.fields.append( [ 'date', fieldNumber ] )
+                    elif not 'amount' in self.fields:
+                        self.fields.append( [ 'amount', fieldNumber ] )
+                    elif not 'memo' in self.fields:
+                        self.fields.append( [ 'memo', fieldNumber ] )
+                    elif not 'name' in self.fields:
+                        self.fields.append( [ 'name', fieldNumber ] )
+                    elif not 'account' in self.fields:
+                        self.fields.append( [ 'account', fieldNumber ] )
+                    elif not 'type' in self.fields:
+                        self.fields.append( [ 'type', fieldNumber ] )
+                    elif not 'debet/credit' in self.fields:
+                        self.fields.append( [ 'debet/credit', fieldNumber ] )
+                        
+    def __init__(self):
+        self.fields = []
             
 
 if __name__ == '__main__':
     config = Config()
     config.checkForConfig()
+    config.getCurrentBank( 'NL94INGB' )
